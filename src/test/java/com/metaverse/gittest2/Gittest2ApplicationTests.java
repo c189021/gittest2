6275b6a64b842d5445e41a8438f084bfd1911da7
SeoungJin Park <sjjw1111@naver.com>
2025-07-01 17:26:06 +0900

    chore: first commit

---
+++ src/test/java/com/metaverse/gittest2/Gittest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.gittest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gittest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
